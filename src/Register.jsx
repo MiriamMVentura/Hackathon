d2558ed7a8f9d7541af45c1ad44ff39fa87a77dd
MiriamMVentura <miriamartv@gmail.com>
2021-05-30 02:26:36 -0500

    feat(form): add input data to firebase

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { db } from './firebase'
+
+
+const initialInputs = {
+    name:'',
+    email:'',
+    date:'',
+    time:''
+}
 
 const Register = () => {
-    return (  
+
+    const [ inputData, setInputData ] = useState(initialInputs);
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        const newObject = ({...inputData, [id]: value});
+        console.log(newObject);
+        setInputData(newObject);
+    }
+
+    const handleSubmit= (e) => {
+        e.preventDefault()
+        db.collection('meets').add(inputData)
+        .then((docRef) => {
+            console.log('id:', docRef.id)
+        }).catch((error) => {
+            console.error('Error adding document:', error);
+        });
+    }
+
+    return (
         <div className='container'>
-            <form>
-            <input type='text' placeholder='Ingresa tu nombre'/>
-            <input type='email' placeholder='Ingresa tu correo'/>
+            <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Ingresa tu nombre' id='name' value={inputData.name} onChange = {handleChange}/>
+            <input type='email' placeholder='Ingresa tu correo' id='email' value={inputData.email} onChange = {handleChange}/>
             <p>Elige tu sucursal</p>
             <p>Elige tu fecha</p>
-            <input type='date'/>
+            <input type='date' id='date' value={inputData.date} onChange = {handleChange}/>
             <p>Elige tu hora</p>
-            <input type='time'/>
-            <input type="submit" value="Confirmar"></input>
+            <input type='time' id='time' value={inputData.time} onChange = {handleChange}/>
+            <input type='submit' value='Confirmar'></input>
             </form>
         </div>
     );
 }
- 
+
 export default Register;
